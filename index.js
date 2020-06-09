6a88238721db13cacfd27d498d77b6a8f9d9129e
HAJJAJINassim <naximo75@gmail.com>
2020-06-09 16:01:00 +0000

    initalize the client app

--- index.js
+++ index.js
@@ -14,11 +14,31 @@ http.listen("4000",function(){
 })
 
 io.on("connection",(socket)=>{
+    
     console.log("a new connection to the room chat");
 
-    socket.userName =userName; // I dont know where this userName came from
-    users.push(socket)
+    socket.emit("loggedIn",{ // emit all users and all messages
+        users:users.map(s=>s.userName),
+        messages:messages
+    })
+
+    socket.on("newUser", userName=>{
+        console.log("new user is connected")
+        socket.userName =userName;                          // I dont know from where this userName came 
+        users.push(socket);
+
+        io.emit("UserOnLine",socket.userName)
+    })
+    socket.on('message',message=>{
+        messages.push({msg:message,user:socket.userNamem,index:index}) //msg content, user who send it, and index represent the order of msg
+        io.emit("msg",message)
+        index++; // for the next msg
+    })
+
+    
+    socket.on("disconnected",()=>{
+        console.log("someone has left the room chat")
+        users.splice(users.indexOf(socket),1) // remove the socket from the array => cause user has left the room chat
+    })
+
 });
-io.on("disconnected",function(){
-    console.log("someone has left the room chat")
-})
